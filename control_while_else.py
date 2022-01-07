6d6090672b81ebe88a0ec1b979a7a9ddbc7b6c35
Shashi Kant <shashikantsantosh@gmail.com>
2022-01-07 21:50:05 +0530

    Initial code

---
+++ control_while_else.py
@@ -0,0 +1,15 @@
+"""
+While Else examples
+"""
+
+# Program to find if a given number is prime or not.
+# A number is prime that cannot be divided by any other number except 1 and itself.
+number = int(input("Enter a number to be checked for prime:"))
+count = 2
+while count < number/2 + 1:
+    if number % count == 0:
+        print("Number is not prime")
+        break
+    count += 1
+else:
+    print("Number is prime")
