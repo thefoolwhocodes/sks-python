895943032bbf3f69751d02e0c2d2591194481ed2
Shashi Kant <shashikantsantosh@gmail.com>
2022-01-09 19:28:13 +0530

    Commit code

--- user-defined-functions/udf_define_global.py
+++ user-defined-functions/udf_define_global.py
@@ -1,10 +1,16 @@
-'''
-Following program demonstrates that global_var_1 will only come into existence when function my_function is called.
-If we comment the call to function, we will get error as 'global_var_1' is not defined
-'''
+"""
+Global keyword
+    Allows to modify a variable outside the current scope.
+    Used to create global variables from a non-global scope like from a function.
+"""
+
+# Following program demonstrates that global_var_1 will only come into existence when function my_function is called.
+# If we comment the call to function, we will get error as 'global_var_1' is not defined
+
+
 def my_function():
     global global_var_1
     global_var_1 = 10
 
-#my_function()
-print (global_var_1)
+# my_function()
+print(global_var_1)
